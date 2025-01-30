1ff536de94d4087a389f61fdc63c7c5fea046e9e
theocgc <theocgc@gmail.com>
2025-01-30 08:03:09 -0300

    Update decrypter.py
    Codigo para descriptografar os arquivos criptografados.

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,24 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+def decrypt_file(file_path, key):
+    with open(file_path, "rb") as file:
+        file_data = file.read()
+    
+    aes = pyaes.AESModeOfOperationCTR(key)
+    decrypt_data = aes.decrypt(file_data)
+    
+    original_file_path = file_path.replace(".ransoncript", "")
+    with open(original_file_path, "wb") as new_file:
+        new_file.write(decrypt_data)
+    
+    os.remove(file_path)
+    print(f"Arquivo {file_path} descriptografado com sucesso!")
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+# Definir chave de descriptografia
+key = b"cripto_bootcamp1"  # 16 bytes
 
-## remover o arquivo criptografado
-os.remove(file_name)
-
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+# Buscar todos os arquivos .ransoncript no diretório atual
+for file_name in os.listdir():
+    if file_name.endswith(".ransoncript"):
+        decrypt_file(file_name, key)
